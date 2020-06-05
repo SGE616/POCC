fb2db2122f99789ac65570e6482d5564e95e2cf4
Shayon Gupta <gshayon122@hotmail.com>
2020-06-06 03:34:12 +0530

    submission for da3

--- app.py
+++ app.py
@@ -2,57 +2,53 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import numpy as np
+import math
+import plotly.graph_objs as go
 
-import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+refractive_indexes = np.array([1.0,1.1,1.2,1.3,1.4,1.5,1.6,1.7,2.0,3.0])
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
+
+    html.H1(children='Refraction characteristics'),
+
+    html.H2(children='''
+        Principles of computer communication
+    '''),
+
     dcc.Graph(id='graph-with-slider'),
+
+    html.Div(children=''),
     dcc.Slider(
-        id='year-slider',
-        min=df['year'].min(),
-        max=df['year'].max(),
-        value=df['year'].min(),
-        marks={str(year): str(year) for year in df['year'].unique()},
+        id='rf-slider',
+        min=refractive_indexes[0],
+        max=refractive_indexes[-1],
+        value=refractive_indexes[0],
+        marks={int(RI) if RI%1==0 else RI : str(RI) for RI in refractive_indexes},
         step=None
-    )
+    ),
 ])
 
 
 @app.callback(
     Output('graph-with-slider', 'figure'),
-    [Input('year-slider', 'value')])
-def update_figure(selected_year):
-    filtered_df = df[df.year == selected_year]
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(dict(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
-
+    [Input('rf-slider', 'value')])
+def update_figure(refInd):
+    ref = 1 # refractive index of air
+    inc_vis = refInd # refractive index of the medium for visible spectrum
+    inc_mil = refInd*2/1.51 # rf of the medium for millimeter wavve
+    crit_vis = math.degrees(math.asin(ref/inc_vis))
+    crit_mil = math.degrees(math.asin(ref/inc_mil))
+    refractive = [go.Bar(x=['visible light','millimeter wave'],y=[crit_vis,crit_mil])]
     return {
-        'data': traces,
+        'data': refractive,
         'layout': dict(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita',
-                   'range':[2.3, 4.8]},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
+            xaxis={'title': 'wavelengths', 'auto-range': True},
+            yaxis={'title': 'critical angle (degrees)', 'auto-range': True},
             hovermode='closest',
             transition = {'duration': 500},
         )
